d418c504b57147ba664f0de959e86ea0420c984d
Robert Conde <rconde2003@gmail.com>
2024-12-25 18:52:43 -0500

    Better info

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 mod gradescope;
 
 use camino::Utf8PathBuf;
-use clap::{Parser, Subcommand};
+use clap::{crate_authors, crate_description, crate_name, crate_version, Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(version, about, long_about=None)]
-#[command(propagate_version = true)]
+#[command(name = crate_name!(), author=crate_authors!())]
+#[command(version=crate_version!(), propagate_version=true)]
+#[command(about="A tool for detecting plagiarism in Gradescope submissions", long_about=crate_description!())]
 struct Cli {
     #[command(subcommand)]
-    command: Commands,
+    command: Command,
 }
 
-#[derive(Subcommand)]
-enum Commands {
+#[derive(Debug, Subcommand)]
+enum Command {
+    #[command(about = "Count the number of submissions in the given export files")]
     Count {
         #[clap(required = true)]
+        #[arg(name = "export files")]
         filepaths: Vec<Utf8PathBuf>,
     },
 }
@@ -23,7 +26,7 @@ fn main() {
     let args = Cli::parse();
 
     match &args.command {
-        Commands::Count { filepaths } => {
+        Command::Count { filepaths } => {
             let mut total_count: usize = 0;
 
             for path in filepaths {
